c91f378f6cf7efb5640055c1757a19f3c0188da5
Huan Chen <hchen.fred@gmail.com>
2016-10-03 20:34:06 -0700

    feat: refactor code to use closure

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,7 +1,32 @@
-var numOfClicks = 0;
-$('#my-elem').click(function(e) {
-  //the element has been clicked... do stuff here
-  //alert("enterhere");
-  numOfClicks++;
-  $('h1').text("clicked " + numOfClicks + " times");
-});
+// cat class
+var Cat = function(img, numOfClicks, elementName) {
+    this.img = img;
+    this.numOfClicks = numOfClicks;
+    this.elementName = elementName;
+};
+
+// create two cats
+var cat1 = new Cat("img/mycat1.jpg", 0, "cat1");
+var cat2 = new Cat("img/mycat2.jpg", 0, "cat2");
+
+var cats = [cat1, cat2];
+
+var htmlTitle = "<h1>Clicked %data% times</h1>";
+var htmlImg = "<button id=%element%><img src=%data% alt='a picture of a cat'></button>";
+var data = "%data%";
+
+
+for (var i = 0; i < cats.length; i++) {
+  var formattedHTMLTitle = htmlTitle.replace(data, cats[i].numOfClicks);
+  var formattedHTMLImg = htmlImg.replace(data, cats[i].img).replace("%element%", cats[i].elementName);
+  var element = '#' + cats[i].elementName;
+  $(element).append(formattedHTMLTitle, formattedHTMLImg);
+  $(element).on('click', (function(){
+    cats[i].numOfClicks++;
+    return function() {    
+      $(element).text("clicked " + cats[i].numOfClicks + " times");
+    };
+  })());
+}
+
+
